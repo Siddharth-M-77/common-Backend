7ce3ea87ea57f128154aa4427035a6266acf247c
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-09-12 03:09:39 -0700

    db connection done

--- DB/db.js
+++ DB/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     console.log(`\n MongoDB connected !! DB HOST :${connectionInstance.connection.host}`);
     
   } catch (error) {
-    console.log("MONGODB connection error", error);
+    console.log("MONGODB connection Faild", error);
     process.exit(1);
   }
 };
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import connectDB from "./DB/db.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config({})
+dotenv.config({ path: "./.env" });
 const app = express();
 
 const PORT = 8000;
@@ -11,9 +11,7 @@ app.get("/", (req, res) => {
   res.send("hello from server");
 });
 
-
-
 app.listen(PORT, () => {
-    connectDB()
+  connectDB();
   console.log(`server is running on the PORT: ${PORT} !!!`);
 });
