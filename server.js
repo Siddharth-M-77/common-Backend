1d532715286bfc47cd4ccff73decc5094a6d98b0
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-09-12 02:56:05 -0700

    database done

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express";
+import connectDB from "./DB/db.js";
+import dotenv from "dotenv"
 
+dotenv.config({})
 const app = express();
 
 const PORT = 8000;
@@ -8,6 +11,9 @@ app.get("/", (req, res) => {
   res.send("hello from server");
 });
 
+
+
 app.listen(PORT, () => {
+    connectDB()
   console.log(`server is running on the PORT: ${PORT} !!!`);
 });
